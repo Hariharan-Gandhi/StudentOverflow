a18960327dff5f2464605ab155acf7c09d46e523
Hariharan Gandhi <hariharan.gandhi@de.ibm.com>
2015-08-06 22:50:50 +0200

    Architecture Diagram

--- www/AdminLTE-2.2.0/index.js
+++ www/AdminLTE-2.2.0/index.js
@@ -1,54 +1,21 @@
-
-
 var username;
 
 $(onStart);
 
 //------------------------------------------------------------------------------
 function onStart() {
-    
+
     var userInput = $("#user-name").val();
-    
+
     username = userInput ? userInput : "Mr.NoName"
-    
-    
+
+
 }
 
-function fetchFromCloudant() {
-    
-    document.getElementById("res").innerHTML = "getCurrentInformation: called";
-    
-    $.ajax({
-        url: "/res/requestCurrentInformation",
-        dataType: "json",
-        success: function(data, status, jqXhr) {
-                document.getElementById("res").innerHTML = "success:" + data[1].key;
+$(document).on("mouseleave","#architecture",function(e){
+   $("[data-widget='collapse']").click();
+});
 
-        },
-        error: function() {
-                document.getElementById("res").innerHTML = "failed: ";
-
-        }
-    });
-    
-}
-
-
-function fetchFromCloudantO() {
-    
-    document.getElementById("res").innerHTML = "getCurrentInformation: called";
-    
-    $.ajax({
-        url: "/res/requestOverallInformation",
-        dataType: "json",
-        success: function(data, status, jqXhr) {
-                document.getElementById("res").innerHTML = "success:<br>" + data.loc_1.name;
-
-        },
-        error: function() {
-                document.getElementById("res").innerHTML = "failed: ";
-
-        }
-    });
-    
-}
+$(document).on("mouseenter","#architecture",function(e){
+   $("[data-widget='collapse']").click();
+});
